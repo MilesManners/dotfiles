128ebd4ba9e62d0796f0340060ecbc7618dc022a
Miles Manners <doctor.miles.manners@gmail.com>
2020-04-01 22:46:05 -0500

    Show polybar on every monitor for now

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main -c $HOME/.config/polybar/config.ini &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload main -c $HOME/.config/polybar/config.ini &
+done
