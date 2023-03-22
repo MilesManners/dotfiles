f8692232bb9552a35c9bd052691764019ff9c3ba
Miles Manners <21149461+MilesManners@users.noreply.github.com>
2023-03-22 12:22:48 -0500

    feat(nvim): configure `gitsigns` mappings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -246,7 +246,50 @@ function M.setup()
 
     use {
       'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},
-      config = function() require('gitsigns').setup() end
+      config = function()
+        require'gitsigns'.setup {
+          on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local wk = require('which-key')
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']h', function()
+              if vim.wo.diff then return ']c' end
+              vim.schedule(function() gs.next_hunk() end)
+              return '<Ignore>'
+            end, {expr=true, desc='next hunk'})
+
+            map('n', '[h', function()
+              if vim.wo.diff then return '[c' end
+              vim.schedule(function() gs.prev_hunk() end)
+              return '<Ignore>'
+            end, {expr=true, desc='prev hunk'})
+
+            -- Actions
+            map({'n', 'v'}, '<leader>gs', ':Gitsigns stage_hunk<CR>', {desc='stage hunk'})
+            map({'n', 'v'}, '<leader>gr', ':Gitsigns reset_hunk<CR>', {desc='reset hunk'})
+            map('n', '<leader>gS', gs.stage_buffer, {desc='stage buffer'})
+            map('n', '<leader>gu', gs.undo_stage_hunk, {desc='undo stage hunk'})
+            map('n', '<leader>gR', gs.reset_buffer, {desc='reset buffer'})
+            map('n', '<leader>gp', gs.preview_hunk, {desc='preview hunk'})
+            map('n', '<leader>gb', function() gs.blame_line{full=true} end, {desc='blame line'})
+            map('n', '<leader>tb', gs.toggle_current_line_blame, {desc='toggle current line blame'})
+            map('n', '<leader>gd', gs.diffthis, {desc='diff this'})
+            map('n', '<leader>gD', function() gs.diffthis('~') end, {desc='diff file'})
+            map('n', '<leader>td', gs.toggle_deleted, {desc='toggle deleted'})
+
+            -- Text object
+            map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', {desc='select hunk'})
+          end
+        }
+      end
     }
 
     use { 'kchmck/vim-coffee-script' }
